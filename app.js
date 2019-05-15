5421761d068ef485dd35d9e52476aaf279790079
arthurhoch <arthur.j.h96@gmail.com>
2019-05-14 23:38:49 -0300

    Add tabela

--- app.js
+++ app.js
@@ -7,21 +7,143 @@ const isTerminal = (symbol) => {
 }
 
 
-const getaBb = (symbol) =>{
+// Tabela final - inicio --- esta pronto
+
+
+const q1 = {
+    S: [['a', 'B'], ['d']],
+    B: [['c', 'D', 'b'], ['empty']],
+    D: [['a'], ['empty']]
+};
+
+const q2 = {
+    Z: [['X', 'Y', 'Z'], ['d']],
+    Y: [['c'], ['empty']],
+    X: [['Y'], ['a']]
+};
+
+const q3 = {
+    S: [['(', 'A', ')'], ['b']],
+    A: [['B', ':', 'A'], ['B']],
+    B: [['a'], ['empty']]
+};
+const q4 = {
+    S: [['(', 'A', ')'], ['b']],
+    A: [['B', 'X']],
+    X: [[';', 'B', 'X'], ['empty']],
+    B: [['a'], ['empty']]
+}
+const q5 = {
+    E: [['T', 'EL'],],
+    EL: [['+', 'T', 'EL'], ['empty']],
+    T: [['F', 'TL']],
+    TL: [['*', 'F', 'TL'], ['empty']],
+    F: [['(', 'E', ')'], ['id']],
+    //         X: [['a']]
+}
+
+tabelaPreditivaTabular = {
+    'E': {
+        'a': ['T', 'EL'],
+    },
+    'EL': {
+        '+': ['+', 'T', 'EL'],
+        '$': ['empty'],
+    },
+    'T': {
+        'a': ['a'],
+    }
+}
+
+escreverTabelafinal = (tabelaFinal) => {
+
+    let tamanho = tabelaFinal.pilha.length;
+
+    console.log("pilha \t\t", "entrada \t\t");
+
+    for (let index = 0; index < tamanho; index++) {
+        console.log("Linha: ", index);
+        console.log("Pilha: ", JSON.stringify(tabelaFinal.pilha[index]));
+        console.log("Entrada: ", JSON.stringify(tabelaFinal.entrada[index]));
+        console.log("Saída: ", JSON.stringify(tabelaFinal.saida[index]));
+        console.log("\n");
+
+    }
+}
+
+
+verificarSentenca = (pilha, entrada, tabelaPreditivaTabular) => {
+    let tabelaFinal = {
+        pilha: [],
+        entrada: [],
+        saida: []
+    }
+
+    tabelaFinal.pilha.push([...pilha]);
+    tabelaFinal.entrada.push([...entrada]);
+    tabelaFinal.saida.push([]);
+
+    let encontrouNaTabela = true;
+
+    while (encontrouNaTabela) {
+        encontrouNaTabela = false;
+
+        let letraEntrada = entrada.shift();
+        let topoPilha = pilha.pop();
+
+        if (letraEntrada === topoPilha && letraEntrada !== '$') {
+            tabelaFinal.pilha.push([...pilha]);
+            tabelaFinal.entrada.push([...entrada]);
+            tabelaFinal.saida.push([]);
+            encontrouNaTabela = true;
+        } else {
+            // Percore as nao terminais
+            for (const naoTerminalTabela in tabelaPreditivaTabular) {
+                if (topoPilha === naoTerminalTabela) {
+                    for (const letraTabela in tabelaPreditivaTabular[topoPilha]) {
+                        if (letraEntrada === letraTabela) {
+                            entrada.unshift(letraEntrada);
+                            let tabelaPreditivaTabularTmp = [...tabelaPreditivaTabular[topoPilha][letraEntrada]];
+                            pilha.push(...tabelaPreditivaTabularTmp.reverse());
+                            pilha = pilha.filter(e => e !== 'empty');
+                            tabelaFinal.pilha.push([...pilha]);
+                            tabelaFinal.entrada.push([...entrada]);
+
+                            let sentenca = {};
+                            sentenca[topoPilha] = [...tabelaPreditivaTabularTmp];
+
+                            tabelaFinal.saida.push(sentenca);
+                            encontrouNaTabela = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    escreverTabelafinal(tabelaFinal);
+}
+
+verificarSentenca(["$", "E"], ["a", "+", "a", "+", "a", "$"], tabelaPreditivaTabular);
+
+// Tabela final - final ----
+
+
+// follow ---------------------------------------------------- Não esta pronto
+const getaBb = (symbol) => {
 
     const aBb = {
     }
 
     let state = START;
 
-    let letterArray =symbol.split("");
+    let letterArray = symbol.split("");
 
 
     letterArray.forEach(letter => {
         if (letter === letter.toLowerCase()) {
             if (state === MIDDLE) {
                 state = END;
-            } 
+            }
             aBb[state] += letter;
         }
         else {
@@ -59,8 +181,8 @@ const follow = (symbols, termial) => {
             termial: aBb[END]
         }
     }
-    
-    if (!isTerminal(aBb[START]) &&  !isTerminal(aBb[MIDDLE])) {
+
+    if (!isTerminal(aBb[START]) && !isTerminal(aBb[MIDDLE])) {
         return {
             first: aBb[START]
         }
@@ -95,166 +217,41 @@ const getFollows = (jsonInput) => {
 
     return follows;
 }
+// follow ----------------------------------------------------
 
 
-(function() {
+
+(function () {
     const q1 = {
-        S: [
-
-            ['a', 'B'],
-
-            ['d']
-
-        ],
-
-        B: [
-
-            ['c', 'D', 'b'],
-
-            ['empty']
-
-        ],
-
-        D: [
-
-            ['a'],
-
-            ['empty'] 
-
-        ]
-
+        S: [['a', 'B'], ['d']],
+        B: [['c', 'D', 'b'], ['empty']],
+        D: [['a'], ['empty']]
     };
 
     const q2 = {
-
-        Z: [
-
-            ['X', 'Y', 'Z'],
-
-            ['d']
-
-        ],
-
-        Y: [
-
-            ['c'],
-
-            ['empty']
-
-        ],
-
-        X: [
-
-            ['Y'],
-
-            ['a']
-
-        ]
-
+        Z: [['X', 'Y', 'Z'], ['d']],
+        Y: [['c'], ['empty']],
+        X: [['Y'], ['a']]
     };
 
     const q3 = {
-
-        S: [
-
-            ['(', 'A', ')'],
-
-            ['b']
-
-        ],
-
-        A: [
-
-            ['B', ':', 'A'],
-
-            ['B']
-
-        ],
-
-        B: [
-
-            ['a'],
-
-            ['empty']
-
-        ]
-
+        S: [['(', 'A', ')'], ['b']],
+        A: [['B', ':', 'A'], ['B']],
+        B: [['a'], ['empty']]
     };
-
     const q4 = {
-
-        S: [
-
-            ['(', 'A', ')'],
-
-            ['b']
-
-        ],
-
-        A: [
-
-            ['B', 'X']
-
-        ],
-
-        X: [
-
-            [';', 'B', 'X'],
-
-            ['empty']
-
-        ],
-
-        B: [
-
-            ['a'],
-
-            ['empty']
-
-        ]
-
+        S: [['(', 'A', ')'], ['b']],
+        A: [['B', 'X']],
+        X: [[';', 'B', 'X'], ['empty']],
+        B: [['a'], ['empty']]
     }
-
     const q5 = {
-
-        E: [
-
-            ['T', 'EL'],
-
-        ],
-
-        EL: [
-
-            ['+', 'T', 'EL'],
-
-            ['empty']
-
-        ],
-
-        T: [
-
-            ['F', 'TL']
-
-        ],
-
-        TL: [
-
-            ['*', 'F', 'TL'],
-
-            ['empty']
-
-        ],
-
-        F: [
-
-            ['(', 'E', ')'],
-
-            ['id'],
-
-        ],
-
-//         X: [['a']]
-
+        E: [['T', 'EL'],],
+        EL: [['+', 'T', 'EL'], ['empty']],
+        T: [['F', 'TL']],
+        TL: [['*', 'F', 'TL'], ['empty']],
+        F: [['(', 'E', ')'], ['id']],
+        //         X: [['a']]
     }
 
     const jsonInput = q4;
@@ -266,7 +263,7 @@ const getFollows = (jsonInput) => {
     const getFirst = (prod) => {
         for (symbol of prod) {
             if (isTerminal(symbol)) {
-                 return symbol;
+                return symbol;
             }
             else {
                 const firsts = [];
@@ -276,7 +273,7 @@ const getFollows = (jsonInput) => {
                 return firsts;
             }
         }
-//         return firsts;
+        //         return firsts;
     }
 
     const treeToArray = (arr, strArrAccumulator = []) => {
@@ -294,15 +291,15 @@ const getFollows = (jsonInput) => {
     const firsts = {};
     for (const key in jsonInput) {
         const productions = jsonInput[key];
-        
+
         let currFirsts = [];
         productions.forEach((prod) => {
             const first = getFirst(prod);
             if (first instanceof Array) {
                 const treeAsArray = treeToArray(first);
-//                 console.log('treeAsArray', treeAsArray);
+                //                 console.log('treeAsArray', treeAsArray);
                 currFirsts = currFirsts.concat(treeAsArray);
-//                 console.log('currFirsts', currFirsts);
+                //                 console.log('currFirsts', currFirsts);
             }
             else {
                 currFirsts.push(first);
